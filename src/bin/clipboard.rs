969496eef2d8c47d92ead802eb7b197d185e2f04
ttimasdf <ttimasdf@users.noreply.github.com>
2020-06-02 18:57:24 +0800

    Add HTML copy support

--- src/bin/clipboard.rs
+++ src/bin/clipboard.rs
@@ -1,21 +1,50 @@
+extern crate clap;
 extern crate clipboard_win;
+extern crate encoding;
+extern crate regex;
+use clap::{App, Arg, ArgGroup};
 use clipboard_win::get_clipboard_string;
+use clipboard_win::raw::register_format;
 use clipboard_win::set_clipboard_string;
+use clipboard_win::Clipboard;
+use regex::Regex;
 
-use std::env;
+use std::fs;
+use std::io::BufRead;
 use std::io::{self, Read, Write};
 
-fn help() {
-    println!("clipboard.exe – Access the Windows clipboard (copy/paste)");
-    println!("");
-    println!("Usage:");
-    println!("  clipboard.exe --copy < echo Hello");
-    println!("  clipboard.exe --paste");
-    println!("");
-    println!("    --copy  - stores stdin into clipboard");
-    println!("    --paste - pastes clipboard content to stdout");
-    println!("");
-    println!("MIT © Sindre Sorhus");
+struct Input<'a> {
+    source: Box<dyn BufRead + 'a>,
+}
+
+impl<'a> Input<'a> {
+    fn console(stdin: &'a io::Stdin) -> Input<'a> {
+        Input {
+            source: Box::new(stdin.lock()),
+        }
+    }
+
+    fn file(path: &str) -> io::Result<Input<'a>> {
+        fs::File::open(path).map(|file| Input {
+            source: Box::new(io::BufReader::new(file)),
+        })
+    }
+}
+
+impl<'a> Read for Input<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.source.read(buf)
+    }
+}
+
+impl<'a> BufRead for Input<'a> {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        self.source.fill_buf()
+    }
+
+    fn consume(&mut self, amt: usize) {
+        self.source.consume(amt);
+    }
 }
 
 fn copy() -> std::io::Result<()> {
@@ -30,19 +59,118 @@ fn paste() -> std::io::Result<()> {
     Ok(())
 }
 
+fn copy_html(mut stream: Input) -> std::io::Result<()> {
+    let mut buffer = String::new();
+    stream.read_to_string(&mut buffer)?;
+
+    let clip = Clipboard::new()?;
+    let format_id = register_format("text/html")?;
+    clip.set(format_id, buffer.as_bytes())?;
+
+    let re = Regex::new(r"<(/?)(html|head|body)[^>]*>").unwrap();
+    let html_buffer = re.replace_all(buffer.as_str(), "");
+    // println!("buffer: {}, buflen: {}", html_buffer, html_buffer.len());
+    let html_formatted = format!(
+        "Version:0.9\r\n\
+        StartHTML:{:08}\r\n\
+        EndHTML:{:08}\r\n\
+        StartFragment:{:08}\r\n\
+        EndFragment:{:08}\r\n\
+        <html><body>\r\n\
+        <!--StartFragment-->{}<!--EndFragment-->\r\n\
+        </body></html>",
+        97,
+        131 + html_buffer.len() + 34,
+        131,
+        131 + html_buffer.len(),
+        html_buffer
+    );
+
+    let format_id_html = register_format("HTML Format")?;
+    clip.set(format_id_html, html_formatted.as_bytes())?;
+
+    println!("{} bytes copied to clipboard!, HTML Format binary size: {}", buffer.len(), html_formatted.len());
+    Ok(())
+}
+
+fn paste_html<W: Write>(mut stream: W) -> std::io::Result<()> {
+    stream.write(&(get_clipboard_string()?).into_bytes())?;
+    Ok(())
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = App::new("clipboard")
+        .version("1.0")
+        .author("ttimasdf")
+        .about("Access the Windows clipboard (copy/paste)")
+        .arg(
+            Arg::with_name("copy")
+                .short("c")
+                .long("copy")
+                .takes_value(false)
+                .help("stores stdin into clipboard"),
+        )
+        .arg(
+            Arg::with_name("paste")
+                .short("p")
+                .long("paste")
+                .takes_value(false)
+                .help("pastes clipboard content to stdout"),
+        )
+        .group(
+            ArgGroup::with_name("mode")
+                .args(&["copy", "paste"])
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .takes_value(true)
+                .help("input file to put into clipboard"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .takes_value(true)
+                .help("output file to store data from clipboard"),
+        )
+        .arg(
+            Arg::with_name("html")
+                .short("H")
+                .long("html")
+                .takes_value(false)
+                .help("manipulate clipboard content as HTML instead of plain text"),
+        )
+        .arg(
+            Arg::with_name("delete")
+                .short("d")
+                .long("delete")
+                .takes_value(false)
+                .help("delete file after copied, useful for automatic pipeline"),
+        )
+        .get_matches();
 
-    if args.len() < 2 {
-        println!("You should specify `--copy` or `--paste` mode. See `--help` for usage examples.");
-        return;
+    let input_filename = args.value_of("input").unwrap_or("-");
+    let stdio = io::stdin();
+    let input: Input = match input_filename {
+        "-" => Input::console(&stdio),
+        _ => Input::file(input_filename).unwrap(),
+    };
+
+    match (args.is_present("copy"), args.is_present("html")) {
+        // copy text
+        (true, false) => copy().expect("Error: Could not copy to clipboard"),
+        // paste text
+        (false, false) => paste().expect("Error: Could not paste from clipboard"),
+        // copy html
+        (true, true) => copy_html(input).expect("Error: Could not copy to clipboard"),
+        // paste html
+        (false, true) => unimplemented!("Not yet available"),
     }
-
-    let cmd = &args[1];
-
-    match &cmd[..] {
-        "--copy" => copy().expect("Error: Could not copy to clipboard"),
-        "--paste" => paste().expect("Error: Could not paste from clipboard"),
-        _ => help(),
+    if args.is_present("delete") {
+        println!("Deleting file after copied");
+        fs::remove_file(input_filename).expect("File cannot be removed");
     }
 }
